d01390bec46763a291c51d4f127ba435f29972ad
Khalid - خالد <1khalidviii@gmail.com>
2024-07-31 00:56:46 +0200

    Removed return and Got the values inside the function

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -3,23 +3,20 @@ let email = document.getElementById("emailstr")
 let comment = document.getElementById("commentstr")
 let form = document.getElementById("comments")
 let btn = document.getElementById("submitbtn")
-let namestr = names.value;
-let emailstr = email.value;
-let commentstr = comment.value;
 alert("Welcome")
 btn.addEventListener("click" , function(event){
     event.preventDefault();
+    let namestr = names.value;
+    let emailstr = email.value;
+    let commentstr = comment.value;
     if(namestr.length === 0){
         alert("Please enter a name");
-        return;
     }
     if(emailstr.length === 0){
         alert("Please enter an email");
-        return;
     }
     if(commentstr.length === 0){
         alert("Please enter your comment");
-        return;
     }
     
 });
